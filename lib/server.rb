f53ca699ecc3560a0f3f3a20f76df94950420587
Max Gasner <max@priorknowledge.com>
2012-06-05 15:46:47 -0700

    app skeleton

---
+++ lib/server.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'activerecord'
+require 'uri'
+
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+ActiveRecord::Base.establish_connection(
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :port     => db.port,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
+)
+
+disable :logging
+set :root, File.dirname(__FILE__) + "/../"
+
+get "/" do
+  File.readlines("public/index.html")
+end
+
+post "/estimate" do
+end
+
+post "/finish" do
+end
