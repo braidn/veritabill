a4f20138f0b140922c2c55d2fec73326ffdc6930
Max Gasner <max@priorknowledge.com>
2012-06-10 19:45:09 -0400

    scaffolding for veritable integration

--- lib/server.rb
+++ lib/server.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'data_mapper'
 require 'uri'
+require 'veritable'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
@@ -17,10 +18,17 @@ end
 
 DataMapper.auto_upgrade!
 
+api = Veritable.connect
+table = api.table 'veritabill'
+
 disable :logging
 set :root, File.dirname(__FILE__) + "/../"
 
 get "/" do
+  ee = estimates
+  ee.each {|e|
+    e['predicted'] = most_recent_analysis.predict
+  }
   # render the app page
   # show a table of past estimates and completions
   # show the most recent estimates, with Veritable estimates and a "complete" button
@@ -43,14 +51,31 @@ post "/complete" do
   # post a completion to the database
   # update the Veritable table
   # rerun analysis
+  if last_analysis_created._id == last_analysis_succeeded._id
+  else
+    n = last_analysis_created._id.split('_')[1].to_i + 1
+    last_analysis_created.delete
+    table.create_analysis(schema, 'veritabill_#{n}')
+  end
 end
 
-def estimates
-  Task.last(10)
+def estimates(params = nil)
+  params.nil? Task.last(10) 
 end
 
 def register_estimate
 end
 
 def register_completion
+end
+
+def connect_to_veritable
+end
+
+def last_analysis_created
+  table.analyses.to_a.max_by {|a| a.created_at}
+end
+
+def last_analysis_succeeded
+  (table.analyses.to_a.select {|a| a.succeeded?}).max_by {|a| a.created_at}
 end
