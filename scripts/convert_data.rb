db0deefad4769323288a3364f484b6e1497d277b
Max Gasner <max@priorknowledge.com>
2012-06-11 08:26:41 -0400

    document scripts

--- scripts/convert_data.rb
+++ scripts/convert_data.rb
@@ -1,3 +1,8 @@
+# Veritabill 1.0.0, June 11, 2012
+# Copyright (c) 2012 Prior Knowledge, Inc.
+# Questions? Contact Max Gasner <max@priorknowledge.com>.
+#
+# Quick script to convert the .csv output by our generative process (/scripts/synthetic.R) into a ruby hash (output to /scripts/seed.rb) that we can easily use to seed our application database
 require 'veritable'
 
 DATASOURCE = "seed_data.csv"
--- scripts/init.rb
+++ scripts/init.rb
@@ -1,6 +1,12 @@
+# Veritabill 1.0.0, June 11, 2012
+# Copyright (c) 2012 Prior Knowledge, Inc.
+# Questions? Contact Max Gasner <max@priorknowledge.com>.
+
+# This script seeds the postgres store with seed data, runs an initial Veritable analysis,  retrieves Veritable estimates of the time tasks will take, and stores them
+
 require 'veritable'
 require 'data_mapper'
-require_relative 'seed'
+require_relative 'seed' # store seed data as a ruby hash
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
@@ -12,28 +18,30 @@ class Task
   property :time_of_day, String
   property :client, String
   property :true_time, Integer
-  property :estimate, Integer
+  property :user_estimate, Integer # user's estimate
+  property :veritable_estimate, Float
 end
 
 DataMapper.auto_migrate!
 
+# We include the user's estimate as a conditioning datum, so Veritable can learn (and correct for) individual user biases
 schema = Veritable::Schema.new({
   'user' => {'type' => 'categorical'},
   'day' => {'type' => 'categorical'},
   'time_of_day' => {'type' => 'categorical'},
   'client' => {'type' => 'categorical'},
   'true_time' => {'type' => 'count'},
-  'estimate' => {'type' => 'count'}
+  'user_estimate' => {'type' => 'count'} 
 })
 records = SEED_DATA
-Veritable::Util.clean_data(records, schema)
+Veritable::Util.clean_data(records, schema) # normalize the data
 
 api = Veritable.connect
-api.tables.each {|t| t.delete}
-t = api.create_table('veritabill', '', {'force' => true})
+api.tables.each {|t| t.delete} # remove existing tables, if any
+t = api.create_table('veritabill')
 
 records.each {|r|
-  Task.create({
+  Task.create({ # add to postgres
     :user => r['user'],
     :day => r['day'],
     :time_of_day => r['time_of_day'],
@@ -41,13 +49,18 @@ records.each {|r|
     :true_time => r['true_time'],
     :estimate => r['estimate']
   })
-  t.upload_row(r)
+  t.upload_row(r) # upload to veritable
 }
 
 a = t.create_analysis(schema, 'veritabill_0')
 a.wait
 
-Task.all.each {|r|
-  estimate = a.predict()
-  r.update()
-}
+# Task.all.each {|r|
+  
+#   r.update({'true_time' => nil})
+#   veritable_estimate = a.predict(r)
+#   Task.update({
+#     :id => r['id'],
+#     :veritable_estimate => veritable_estimate['true_time']  
+#   })
+# }
